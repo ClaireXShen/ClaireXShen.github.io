0d2c009ab917e86a7dd5cd6735bb4c98381f251c
ClaireXShen <85016798+ClaireXShen@users.noreply.github.com>
2023-05-17 14:28:21 +0800

    Delete home.js

--- docs/home.js
+++
@@ -1,8 +0,0 @@
-const currentLocation = location.href;
-const navLinks = document.querySelectorAll("nav ul li a");
-const navLength = navLinks.length;
-for (let i = 0; i < navLength; i++) {
-  if (navLinks[i].href === currentLocation) {
-    navLinks[i].classList.add("active");
-  }
-}
